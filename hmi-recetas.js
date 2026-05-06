8b3e7ceb0830372536c23660926a7ab5dbb34351
haleon-arg <sebadegraf@hotmail.com>
2026-05-05 22:12:24 -0300

    Add files via upload

---
+++ hmi-recetas.js
@@ -0,0 +1,115 @@
+var HMI_RECETAS = {
+  'boato1': {
+    'sec': {
+      'andrews': {
+        'x50': {
+          label: 'Andrews x50',
+          fotos: [
+            {n:'01.jpg', t:'Recetas - Estuches x50'},
+            {n:'02.jpg', t:'Estado Maquina - Velocidad y Contadores'},
+            {n:'03.jpg', t:'Config. Interfaz - Ajustes Banda Transportadora'},
+            {n:'04.jpg', t:'Config. Interfaz - Ajustes Acumulador'},
+            {n:'05.jpg', t:'Config. Interfaz - Ajustes Empujador y Transporte'},
+            {n:'06.jpg', t:'Config. Interfaz - Ajustes Apiladores'},
+            {n:'07.jpg', t:'Config. Interfaz - Ajustes Cangilones Agrupadores'},
+            {n:'08.jpg', t:'Ajustes Encartonadora - Fases 1-8'},
+            {n:'09.jpg', t:'Ajustes Encartonadora - Fases 9-16'},
+            {n:'10.jpg', t:'Ajustes Encartonadora - Fases 17-24'},
+            {n:'11.jpg', t:'Ajustes Encartonadora - Fases 25-28'},
+            {n:'12.jpg', t:'Ajustes Encartonadora - Cadenas'},
+            {n:'13.jpg', t:'Ajustes Encartonadora - Empujador'},
+            {n:'14.jpg', t:'Ajustes Encartonadora - Empujador Externo'},
+            {n:'15.jpg', t:'Ajustes Encartonadora - Doblador Solapas'},
+            {n:'16.jpg', t:'Ajustes Cajas - Ajustes de Cajas'},
+            {n:'17.jpg', t:'Ajustes Cajas - Ajustes de la Rueda'},
+            {n:'18.jpg', t:'Ajustes Cajas - Manejo de las Cajas'},
+            {n:'19.jpg', t:'Sistema de Vision'},
+            {n:'20.jpg', t:'Recetas - Estuches x12'},
+            {n:'21.jpg', t:'Banda de Salida'}
+          ]
+        }
+      }
+    },
+    'prim': {
+      'andrews_clasico': {
+        label: 'Andrews Clasico',
+        folder: 'andrews_x50',
+        fotos: [
+          {n:'01.jpg', t:'Principal - Velocidad y Temperaturas'},
+          {n:'02.jpg', t:'Dosificador - Barrena de Llenado'},
+          {n:'03.jpg', t:'Dosificador - Configuracion de Balanza'},
+          {n:'04.jpg', t:'Dosificador - Cut-Off'},
+          {n:'05.jpg', t:'Dosificador - Opciones de Polvos'},
+          {n:'06.jpg', t:'Dosificador - Control en Linea (Bobina)'},
+          {n:'07.jpg', t:'Dosificador - Agitadores'},
+          {n:'08.jpg', t:'Dosificador - Control de Nivel'},
+          {n:'09.jpg', t:'Impresora'},
+          {n:'10.jpg', t:'Cuchilla - Rodillos y Corte'},
+          {n:'11.jpg', t:'Empaque - Muestreo y Configuracion'},
+          {n:'12.jpg', t:'Marca y Vision'},
+          {n:'13.jpg', t:'Resistencia - Rodillos y Mordazas'},
+          {n:'14.jpg', t:'Recetas - Pagina de Recetas (x50)'},
+          {n:'15.jpg', t:'Recetas - sal and. x12'},
+          {n:'16.jpg', t:'Recetas - sal and. x50'},
+          {n:'17.jpg', t:'Recetas - Centrum x12'}
+        ]
+      },
+      'andrews_sabor': {
+        label: 'Andrews Sabor',
+        folder: 'andrews_sabor',
+        fotos: [
+          {n:'01.jpg', t:'Principal - Velocidad y Temperaturas'},
+          {n:'02.jpg', t:'Dosificador - Barrena de Llenado'},
+          {n:'03.jpg', t:'Dosificador - Configuracion de Balanza'},
+          {n:'04.jpg', t:'Dosificador - Cut-Off'},
+          {n:'05.jpg', t:'Dosificador - Opciones de Polvos'},
+          {n:'06.jpg', t:'Dosificador - Control en Linea (Bobina)'},
+          {n:'07.jpg', t:'Dosificador - Agitadores'},
+          {n:'08.jpg', t:'Dosificador - Control de Nivel'},
+          {n:'09.jpg', t:'Impresora'},
+          {n:'10.jpg', t:'Cuchilla - Rodillos y Corte'},
+          {n:'11.jpg', t:'Empaque - Muestreo y Configuracion'},
+          {n:'12.jpg', t:'Marca y Vision'},
+          {n:'13.jpg', t:'Resistencia - Rodillos y Mordazas'},
+          {n:'14.jpg', t:'Recetas - Pagina de Recetas'}
+        ]
+      },
+      'centrum': {
+        label: 'Centrum',
+        folder: 'centrum',
+        fotos: []
+      }
+    }
+  },
+  'marchesini': {
+    'prim': {
+      'llenadora_e632_x12': {
+        label: 'Llenadora E-632 x12',
+        folder: 'llenadora_e632_x12',
+        fotos: [
+          {n:'01.jpg', t:'Modificacion Formato - Pantalla 1'},
+          {n:'02.jpg', t:'Modificacion Formato - Pantalla 2'},
+          {n:'03.jpg', t:'Modificacion Formato - Pantalla 3'},
+          {n:'04.jpg', t:'Modificacion Formato - Pantalla 4'}
+        ]
+      }
+    },
+    'sec': {}
+  },
+  'boato2': {
+    'prim': {},
+    'sec': {}
+  },
+  'sig1': {
+    'prim': {},
+    'sec': {}
+  },
+  'sig2': {
+    'prim': {},
+    'sec': {}
+  },
+  'tover': {
+    'prim': {},
+    'sec': {}
+  }
+};
